26559acf59def88133bbb36dce45b8101fa988d0
Jayce Bordelon <b.jayce@wustl.edu>
2025-02-12 12:47:48 -0600

    Ticket Master Api decoupling & UI rendering skely

--- seat-sleuth-client/src/api/test/test.js
+++
@@ -1,3 +0,0 @@
-fetch(`https://app.ticketmaster.com/discovery/v2/events.json?size=1&keyword=concert&postalCode=63130&apikey=${import.meta.env.VITE_TM_API_KEY}`)
-.then(res => console.info(res))
-.catch(err => console.error(err));
