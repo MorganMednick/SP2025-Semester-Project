2b1e0219a7fe320c78ff9462264a54042c6a818c
sammyfalvey <falvey.s@wustl.edu>
2025-02-17 19:24:40 -0600

    about section

--- seat-sleuth-client/src/components/home/About.tsx
+++ seat-sleuth-client/src/components/home/About.tsx
@@ -1,21 +1,42 @@
-import { Card, Center, Container, Divider, Stack, Title } from '@mantine/core';
+import { Text, Center, Container, Divider, Stack, Title, Group, Image, Paper } from '@mantine/core';
+import SearchBar from '../search/Search';
+import { seatGeekLogo, stubHubLogo, ticketMasterLogo } from '../../util/assetReconcileUtil';
 
-// TODO: Make this actually render out our figma!! 1. Make a search bar component for the home page that replaces this card
 export default function About() {
   return (
-    <Container bg="green.4" fluid w="100%">
-      <Container p={40}>
-        <Stack gap={40}>
-          <Center>
-            <Title c="white" order={1} component="h1" size={46}>
+    <Container bg="green.7" fluid w="100%">
+      <Container p={40} maw={1400}>
+        <Stack justify="center">
+          <Center p={30}>
+            <Title c="white" order={1}>
               Watch ticket prices across platforms
             </Title>
           </Center>
           <Center>
-            {/* TODO: Replace card with search bar */}
-            <Card w={800}></Card>
+            <SearchBar width={1000} size={'xl'}></SearchBar>
           </Center>
-          <Divider />
+          <Center py={30}>
+            <Divider size="md" w={'100%'} color="green.8" />
+          </Center>
+          <Title c="white" order={2}>
+            Our philosophy
+          </Title>
+          <Text c="white">
+            When it comes to resale tickets to concerts and sports games, there are a lot of
+            options. You can compare them all, but then... there are the fees. That’s why we exist!
+            We compile prices from all the major sites— fees included. We’ll even notify you when
+            prices drop! Never worry about getting the best price again.{' '}
+          </Text>
+          <Paper bg="green.9" mt={20}>
+            <Group p={20} gap="10%">
+              <Text c="white" size="lg" ml={20}>
+                Comparing prices from...
+              </Text>
+              <Image src={ticketMasterLogo} alt="Ticketmaster logo" />
+              <Image src={stubHubLogo} alt="StubHub logo" />
+              <Image src={seatGeekLogo} alt="SeakGeek logo" />
+            </Group>
+          </Paper>
         </Stack>
       </Container>
     </Container>
--- seat-sleuth-client/src/components/search/Search.tsx
+++ seat-sleuth-client/src/components/search/Search.tsx
@@ -1,13 +1,19 @@
 import { TextInput } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
 
-export default function SearchBar() {
+interface SearchProps {
+  width?: number;
+  size?: string;
+}
+
+export default function SearchBar({ width, size }: SearchProps) {
   const icon = <IconSearch stroke={2} height={20} />;
   return (
     <TextInput
       leftSectionPointerEvents="none"
       leftSection={icon}
-      size="md"
+      size={size}
+      w={width}
       placeholder="Search for an event..."
     />
   );
