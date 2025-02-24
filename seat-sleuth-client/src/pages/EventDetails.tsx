e2ff01db136134f5d53aec888f4d06d3507ac8a3
Bhalla-V <bhalla.v@wustl.edu>
2025-02-24 12:50:59 -0600

    do work in EventDetails.tsx for fetching event data. Outline of code is set u p, need to deconstruct array

---
+++ seat-sleuth-client/src/pages/EventDetails.tsx
@@ -0,0 +1,28 @@
+import { Card, Image, Text, Button, Container } from '@mantine/core';
+import { EventData } from '@shared/api/external/eventData';
+import { TicketMasterSearchParams } from '@shared/api/external/ticketMaster';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { fetchTicketMasterEvents } from '../api/functions/ticketMaster';
+export default function EventDetails() {
+  const { id } = useParams();
+  const {
+    data: events,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<EventData[], Error>(['ticketMasterEvents', id], async () => {
+    const params: TicketMasterSearchParams = {
+      id,
+    };
+    const res = await fetchTicketMasterEvents(params);
+    return res.data || [];
+  });
+  return (
+    <Container size="lg" py="xl">
+      {JSON.stringify(events)}
+    </Container>
+  );
+}
+
+//TODO: MORGAN Pass the parsed data from homepage to this eventpage.
