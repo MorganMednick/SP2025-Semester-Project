a72ca08c042a4b0f96968b39953761c93d4749db
Bhalla-V <bhalla.v@wustl.edu>
2025-02-26 23:06:07 -0600

    updated to use components and top down approach, also made more like figma

--- seat-sleuth-client/src/components/events/EventDetailsTicketCard.tsx
+++ seat-sleuth-client/src/components/events/EventDetailsTicketCard.tsx
@@ -1,4 +1,69 @@
-// TODO
-export default function EventDetailsTicketCard() {
-  return <div>EventDetailsTicketCard</div>;
+import { Text,Card,Group,Anchor,Flex } from '@mantine/core';
+
+
+
+interface TicketProps {
+  priceMin?: number | null;
+  url?: string | null;
+}
+
+
+export default function EventDetailsTicketCard({priceMin,url}: TicketProps) {
+  return (
+    <div>
+      <Flex justify="flex-end" pr={50} gap="xl" mt={-10} mb={10}>
+        {priceMin && url && (
+          <Card
+            withBorder
+            radius="md"
+
+            w={251}
+            h={180}
+            style={{ borderColor: '#49905F', borderWidth: 2 }}
+          >
+            <Text size="xxxx" fw={700} c="green.7" ta="center" mt={-10}>
+              ${priceMin}
+            </Text>
+            <Group justify="center" mt={-10}>
+              <Anchor href={url || undefined} target="_blank" c="green.7" size="xx">
+                TicketMaster
+              </Anchor>
+            </Group>
+          </Card>
+        )}
+        <Card
+          withBorder
+          radius="md"
+          w={251}
+          h={180}
+          style={{ borderColor: '#E49648', borderWidth: 2 }}
+        >
+          <Text size="xxxx" fw={700} c="#E49648" ta="center" mt={-10}>
+            $49.99
+          </Text>
+          <Group justify="center" mt={-10}>
+            <Anchor href={url || undefined} target="_blank" c="#E49648" size="xx">
+              SeatGeek
+            </Anchor>
+          </Group>
+        </Card>
+        <Card
+          withBorder
+          radius="md"
+          w={251}
+          h={180}
+          style={{ borderColor: '#BD3133', borderWidth: 2 }}
+        >
+          <Text size="xxxx" fw={700} c="#BD3133" ta="center" mt={-10}>
+            ${69.69}
+          </Text>
+          <Group justify="center" mt={-10}>
+            <Anchor href={url || undefined} target="_blank" c="#BD3133" size="xx">
+              StubHub
+            </Anchor>
+          </Group>
+        </Card>
+      </Flex>
+    </div>
+  );
 }
--- seat-sleuth-client/src/pages/EventDetails.tsx
+++ seat-sleuth-client/src/pages/EventDetails.tsx
@@ -1,26 +1,19 @@
 import {
-  Container,
   Text,
-  Image,
-  Flex,
-  Title,
-  Group,
-  Card,
-  Anchor,
-  Checkbox,
-  Stack,
-  Divider,
-  NativeSelect,
 } from '@mantine/core';
 import { Event } from '@shared/api/responses';
 import { TicketMasterSearchParams } from '@shared/api/external/ticketMaster';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { fetchTicketMasterEvents } from '../api/functions/ticketMaster';
+import EventDetailsImageSection from '../components/events/EventDetailsImageSection';
+import EventDetailsTicketCard from '../components/events/EventDetailsTicketCard';
+import EventDetailsInfoSection from '../components/events/EventDetailsInfoSection';
 
 export default function EventDetails() {
   const { id } = useParams();
-  const { data: events } = useQuery<Event[], Error>(['ticketMasterEvents', id], async () => {
+  const { data: events,
+ } = useQuery<Event[], Error>(['ticketMasterEvents', id], async () => {
     const params: TicketMasterSearchParams = { id };
     const res = await fetchTicketMasterEvents(params);
     return res.data || [];
@@ -32,101 +25,16 @@ export default function EventDetails() {
 
   const event = events[0];
 
-  const formattedDate = new Date(event.startTime).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-
   return (
-    <Container fluid w="100%" p={0} m={0}>
-      {/* TODO: Replace with EventDetailsImageSection */}
-      {event.imageSrc && event.name && (
-        <Container fluid w="100%" p={0} m={0}>
-          <Image src={event.imageSrc[0]} alt={event.name} height={400} width="100%" />
-
-          <Flex justify="space-between" align="center">
-            <Title c="white" order={1}>
-              {event.name}
-            </Title>
-            <Checkbox
-              color="green"
-              iconColor="white"
-              label="Watch price"
-              labelPosition="left"
-              c="white"
-            />
-          </Flex>
-        </Container>
-      )}
-
-      {/* TODO: Replace with EventDetailsInfoSection */}
-      <Flex direction={{ base: 'column', sm: 'row' }} align="center" gap="xl" mt="lg">
-        {/* Location & Section Selectors */}
-        <Stack gap="lg">
-          <NativeSelect label="Location" size="md" data={['Option 1', 'Option 2']} w={300} />
-          <NativeSelect label="Section" size="md" data={['Option A', 'Option B']} w={300} />
-        </Stack>
-
-        {/* Event Date */}
-        <Text size="lg" tt="uppercase" ta="right">
-          {formattedDate}
-        </Text>
-
-        {/* Vertical Divider */}
-        <Divider size="lg" color="black" orientation="vertical" h={100} />
-
-        {/* Venue Info */}
-        <Stack gap="xs">
-          <Text size="md" fw={700}>
-            {event.city}
-          </Text>
-          <Text size="sm" tt="uppercase">
-            {event.venueName}
-            {event.venueSeatMapSrc && (
-              <Anchor href={event.venueSeatMapSrc} target="_blank" size="sm" td="underline">
-                See map
-              </Anchor>
-            )}
-          </Text>
-        </Stack>
-      </Flex>
-
-      {/* TODO: Replace with EventDetailsTicketCard */}
-      <Flex justify="center" gap="xl" mt="lg">
-        {event.priceMin && event.url && (
-          <Card withBorder radius="md">
-            <Text size="xxl" fw={700}>
-              ${event.priceMin}
-            </Text>
-            <Group justify="center">
-              <Anchor href={event.url || undefined} target="_blank" c="green.7" size="xl">
-                TicketMaster
-              </Anchor>
-            </Group>
-          </Card>
-        )}
-        <Card withBorder radius="md">
-          <Text size="xxl" fw={700}>
-            ${69.69}
-          </Text>
-          <Group justify="center">
-            <Anchor href={event.url || undefined} target="_blank" c="orange" size="xl">
-              SeatGeek
-            </Anchor>
-          </Group>
-        </Card>
-        <Card withBorder radius="md">
-          <Text size="xxl" fw={700}>
-            ${69.69}
-          </Text>
-          <Group justify="center">
-            <Anchor href={event.url || undefined} target="_blank" c="purple" size="xl">
-              StubHub
-            </Anchor>
-          </Group>
-        </Card>
-      </Flex>
-    </Container>
+    <>
+      <EventDetailsImageSection name={event.name} image={event.imageSrc} />
+      <EventDetailsInfoSection
+        startTime={event.startTime}
+        venueMap={event.venueSeatMapSrc}
+        venue={event.venueName}
+        city={event.city}
+      />
+      <EventDetailsTicketCard priceMin={event.priceMin} url={event.url} />
+    </>
   );
 }
