c4f3f254fcad006818db79d93ecd705d3380101a
Jayce Bordelon <b.jayce@wustl.edu>
2025-03-03 07:39:28 -0600

    Cleaning up docker scripts for error handling and db migration simplification

--- start.sh
+++ start.sh
@@ -2,20 +2,15 @@ set -e
 
 
 cleanup_containers() {
-    echo -e "\n🔻 Forcing removal of lingering containers...\n"
-    docker ps -aq | xargs -r docker rm -f
-
-    echo -e "\n🔻 Stopping and removing existing Docker containers...\n"
+    echo -e "\nStopping and removing existing Docker containers...\n"
     docker-compose down -v --remove-orphans || true
-
-    echo -e "\n🗑️  Removing unused volumes...\n"
     docker volume prune -f
 }
 
 
 
 start_containers() {
-    echo -e "\n🦥 Starting Up Seat Sleuth Containers...\n"
+    echo -e "\nStarting Up Seat Sleuth Containers...\n"
     docker-compose up --build -d
 }
 
@@ -23,18 +18,20 @@ start_containers() {
 wait_for_health_check() {
     local HEALTH_CONTAINER_ID=$(docker ps -aqf "name=health")
     if [ -n "$HEALTH_CONTAINER_ID" ]; then
-        echo -e "\n🔥 Health Check Complete! All Services Healthy..."
-        echo -e "\n🗑️  Removing Vestigial Health Check container for memory optimization (ID: $HEALTH_CONTAINER_ID)..."
+        echo -e "\nHealth Check Complete! All Services Healthy..."
+        echo -e "\nRemoving Vestigial Health Check container for memory optimization (ID: $HEALTH_CONTAINER_ID)..."
         docker rm -f "$HEALTH_CONTAINER_ID" > /dev/null 2>&1
     fi
 }
 
 
 display_service_urls() {
-    echo -e "\n🚀 Seat Sleuth is Up and Running! 🦥\n"
-    echo -e "\t✅ Client:        http://localhost:5173"
-    echo -e "\t✅ Server:        http://localhost:4000"
-    echo -e "\t✅ Prisma Studio: http://localhost:5555\n"
+    echo -e "\tClient:        http://localhost:5173"
+    echo -e "\tServer:        http://localhost:4000"
+}
+
+follow_logs() {
+    docker-compose logs --follow
 }
 
 
@@ -42,3 +39,4 @@ cleanup_containers
 start_containers
 wait_for_health_check
 display_service_urls
+follow_logs
