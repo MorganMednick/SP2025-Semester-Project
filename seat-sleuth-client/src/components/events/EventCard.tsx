ca3dd693d795209fd0c7783204d9870775fa81e2
Jayce Bordelon <b.jayce@wustl.edu>
2025-03-05 16:00:47 -0600

    Auto Synch

--- seat-sleuth-client/src/components/events/EventCard.tsx
+++ seat-sleuth-client/src/components/events/EventCard.tsx
@@ -8,27 +8,24 @@ export default function EventCard({ event }: { event: EventWithOptions }) {
   const { hovered, ref } = useHover();
 
   return (
-    <Card
-      ref={ref}
-      shadow="sm"
-      padding="lg"
-      radius="md"
-      withBorder
-      w="100%"
-      onClick={() => navigate(`/events/${event.eventName}`)}
-      style={{ cursor: 'pointer' }}
-    >
+    <Card ref={ref} shadow="sm" padding="lg" radius="md" withBorder w="100%">
       <Card.Section>
         <Image
           src={event?.imageSrc?.[0] || 'https://picsum.photos/400'}
           height={160}
           alt={event.eventName}
         />
-        <Tooltip label={event.eventName} position="top" color="green" withArrow>
+        <Tooltip
+          label={event.eventName}
+          position="top"
+          color="green"
+          withArrow
+          transitionProps={{ duration: 200, transition: 'pop' }}
+        >
           <Overlay
-            zIndex={0}
-            gradient="linear-gradient(145deg, rgba(0, 0, 0, 0.95) 5%, rgba(0, 0, 0, 0) 50%)"
+            gradient="linear-gradient(145deg, rgba(0, 0, 0, 0.95) 10%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0) 90%)"
             opacity={hovered ? 1.0 : 0.95}
+            onClick={() => navigate(`/events/${event.eventName}`)}
           >
             <Center w="100%" h="100%" px={12}>
               <Text c="white" size="xl" fw={600} ta="center" lineClamp={2}>
