ed32aa8509007726f6cf1b21fa5cafca45c2f7b3
Jayce Bordelon <b.jayce@wustl.edu>
2025-03-15 21:22:40 -0500

    Cleaning sanitization logic

--- seat-sleuth-client/src/pages/EventDetails.tsx
+++ seat-sleuth-client/src/pages/EventDetails.tsx
@@ -2,32 +2,28 @@ import { ApiResponse, EventData, TicketMasterQueryResponse } from '@shared/api/r
 import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { fetchTicketMasterEvents } from '../api/functions/ticketMaster';
-import { useMemo, useEffect } from 'react';
-import { unsanitizeEventName, sanitizeEventName } from '../util/sanitization';
+import { useEffect } from 'react';
 import PageLayout from '../components/layout/PageLayout';
+import { sanitizeEventName, unsanitizeEventName } from '../util/sanitization';
 
 export default function EventDetails() {
   const { name, id } = useParams();
+  const sanitizedName = sanitizeEventName(name || '');
   const navigate = useNavigate();
 
-  const formattedName = useMemo(
-    () => (name ? encodeURIComponent(unsanitizeEventName(name)) : ''),
-    [name],
-  );
-
   useEffect(() => {
-    if (name && name !== sanitizeEventName(decodeURIComponent(formattedName))) {
-      const newUrl = `/events/${sanitizeEventName(decodeURIComponent(formattedName))}${id ? `/${id}` : ''}`;
-      if (newUrl !== window.location.pathname) {
-        navigate(newUrl, { replace: true });
+    if (name) {
+      if (sanitizedName !== name) {
+        const newUrl = `/events/${sanitizedName}${id ? `/${id}` : ''}`;
+        if (newUrl !== window.location.pathname) {
+          navigate(newUrl, { replace: true });
+        }
       }
     }
-  }, [name, id, navigate, formattedName]);
-
-  const queryKey = useMemo(() => ['eventWithOptions', formattedName, id], [formattedName, id]);
+  }, [name]);
 
   const { data: event } = useQuery<EventData | null, Error>(
-    queryKey,
+    ['eventWithOptions', name, id],
     async () => {
       if (id) {
         const res: ApiResponse<TicketMasterQueryResponse> = await fetchTicketMasterEvents({
@@ -36,21 +32,20 @@ export default function EventDetails() {
         return res?.data?.[0] ?? null;
       }
 
-      if (formattedName) {
-        const res = await fetchTicketMasterEvents({ keyword: decodeURIComponent(formattedName) });
+      if (name) {
+        const res = await fetchTicketMasterEvents({ keyword: unsanitizeEventName(name) });
         return res?.data?.[0] ?? null;
       }
 
       return null;
     },
     {
-      enabled: !!formattedName || !!id,
+      enabled: (!!name && sanitizedName === name) || !!id,
       staleTime: 1000 * 60 * 5,
       cacheTime: 1000 * 60 * 30,
       refetchOnMount: false,
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
-      retry: false,
       onError: () => {
         navigate('/404');
       },
--- seat-sleuth-client/src/util/sanitization.ts
+++ seat-sleuth-client/src/util/sanitization.ts
@@ -1,4 +1,4 @@
-const SAFE_CHAR_REGEX = /[^a-zA-Z0-9-_'& ]/g;
+const SAFE_CHAR_REGEX = /[^a-zA-Z0-9-_'&\- ]/g;
 
 export function sanitizeEventName(name: string): string {
   return name.trim().replace(/ +/g, '-').replace(/\//g, '_').replace(SAFE_CHAR_REGEX, '');
